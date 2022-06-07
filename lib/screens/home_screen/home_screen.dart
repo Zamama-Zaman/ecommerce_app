c5b259d63476f610e8b867cb44252acbde9de1d6
zamama-zaman <zamamazaman@gmail.com>
2022-06-07 23:39:40 +0500

    first commit

---
+++ lib/screens/home_screen/home_screen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const routeName = "home-screen";
+  // const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Home Screen"),
+      ),
+    );
+  }
+}
---
+++ lib/screens/sign_up/components/body.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:nidhub_app/components/socal_card.dart';
+// import 'package:shop_app/components/socal_card.dart';
+// import 'package:shop_app/constants.dart';
+// import 'package:shop_app/size_config.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+import 'sign_up_form.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.001), // 4%
+                Text("Register Account", style: headingStyle),
+                Text(
+                  "Complete your details or continue \nwith Nidhub",
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SignUpForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.04),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocalCard(
+                      icon: "assets/icons/google-icon.svg",
+                      press: () {},
+                    ),
+                    SocalCard(
+                      icon: "assets/icons/facebook-2.svg",
+                      press: () {},
+                    ),
+                    SocalCard(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                Text(
+                  'By continuing your confirm that you agree \nwith our Term and Condition',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.caption,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
